d3d59fc812cf46ae21ab3fb24edb9832c125e259
its-me-ojas <crestfallenwizard@gmail.com>
2023-11-09 01:31:49 +0530

    window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod chacha20;
 
+use gtk::prelude::*;
+use gtk::{Button, FileChooserAction, FileChooserDialog, Label, ResponseType, Window, WindowType};
 use std::fs::File;
 use std::io::{Read, Write};
 
@@ -19,6 +21,20 @@ fn main() {
     // Replace these with your actual key and nonce
     let key = [0u8; 32];
     let nonce = [0u8; 8];
+
+    // gtk initialization
+    gtk::init().expect("Failed to initialize GTK.");
+
+    // create a new top level window
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("File Encryption tool");
+    window.set_default_size(400, 200);
+
+    // create label and button
+    let input_label = Label::new(Some("Input File:"));
+    let output_label = Label::new(Some("Output File:"));
+    let encrypt_button = Button::with_label("Encrypt:");
+    let decrypt_button = Button::with_label("Decrypt:");
 
     // Example usage of the ChaCha20 encryption function
     let input_path = "/home/crestfallen/Documents/secure-file-encryption/public/pfp.jpeg";
